b099f6a0b8a33f04859ecdb08491d3f64749935a
Elias Wald <elias@fwix.com>
2011-06-03 12:19:27 -0700

    fourth commit

--- ControlTest/Classes/ControlTestViewController.h
+++ ControlTest/Classes/ControlTestViewController.h
@@ -7,16 +7,30 @@
 //
 
 #import <UIKit/UIKit.h>
-
-@interface ControlTestViewController : UIViewController {
+#define kShowSegmentIndex 0
+@interface ControlTestViewController : UIViewController <UIActionSheetDelegate> {
 	IBOutlet UITextField *cityField;
 	IBOutlet UITextField *stateField;
+	IBOutlet UILabel *sliderLabel;
+	IBOutlet UISwitch *leftSwitch;
+	IBOutlet UISwitch *rightSwitch;
+	IBOutlet UIView *switchView;
+	IBOutlet UIButton *doSomethingButton;
 }
 @property (nonatomic, retain) UITextField *cityField;
 @property (nonatomic, retain) UITextField *stateField;
+@property (nonatomic, retain) UILabel *sliderLabel;
+@property (nonatomic, retain) UISwitch *leftSwitch;
+@property (nonatomic, retain) UISwitch *rightSwitch;
+@property (nonatomic, retain) UIView *switchView;
+@property (nonatomic, retain) UIButton *doSomethingButton;
 
 - (IBAction)textFieldDoneEditing:(id)sender;
 - (IBAction)backgroundClick:(id)sender;
+- (IBAction)sliderChanged:(id)sender;
+- (IBAction)switchChanged:(id)sender;
+- (IBAction)toggleShowHide:(id)sender;
+- (IBAction)buttonClicked:(id)sender;
 
 @end
 
--- ControlTest/Classes/ControlTestViewController.m
+++ ControlTest/Classes/ControlTestViewController.m
@@ -7,44 +7,77 @@
 //
 
 #import "ControlTestViewController.h"
+#import "fAPI.h"
 
 @implementation ControlTestViewController
-@synthesize cityField, stateField;
+@synthesize cityField, stateField, sliderLabel, leftSwitch, rightSwitch, switchView, doSomethingButton;
 
-
-/*
-// The designated initializer. Override to perform setup that is required before the view is loaded.
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+- (IBAction)buttonClicked:(id)sender
+{
+	UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"Are you sure?" delegate:self cancelButtonTitle:@"Nope" destructiveButtonTitle:@"Yes, I'm sure" otherButtonTitles:nil];
+	[actionSheet showInView:self.view];
+	[actionSheet release];
 }
-*/
 
-/*
-// Implement loadView to create a view hierarchy programmatically, without using a nib.
-- (void)loadView {
+- (void)actionSheet:(UIActionSheet *)actionSheet didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+	if (!(buttonIndex == [actionSheet cancelButtonIndex])) {
+		NSString *msg = @"";
+		NSString *place = @"";
+		
+		fAPI *myfAPI = [[fAPI alloc] initWithKey:@"00039d497003" userID:nil];
+		fPlace *placeInput = [[fPlace alloc] init];
+		NSArray *placesNearCity = [[NSArray alloc] init];
+		placesNearCity = [myfAPI getPlacesByCity:cityField.text province:stateField.text error:nil];
+		for (int i = 0; i<[placesNearCity count]; i++) {
+			placeInput = [placesNearCity objectAtIndex:i];
+			place = [placeInput.name stringByAppendingString:@"\n"];;
+			msg = [msg stringByAppendingString:place];
+		}
+		
+		NSString *title = [[NSString alloc] initWithFormat:@"Places near %@", cityField.text];
+		
+		UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title message:msg delegate:nil cancelButtonTitle:@"Done" otherButtonTitles:nil];
+		[alert show];
+		[place release];
+		[myfAPI release];
+		[placeInput release];
+		[placesNearCity release];
+		[alert release];
+		[msg release];
+	}
 }
-*/
 
-
-/*
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)viewDidLoad {
-    [super viewDidLoad];
+- (IBAction)switchChanged:(id)sender
+{
+	UISwitch *whichSwitch = (UISwitch *)sender;
+	BOOL setting = whichSwitch.isOn;
+	[leftSwitch setOn:setting animated:YES];
+	[rightSwitch setOn:setting animated:YES];
 }
-*/
 
-
-/*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+- (IBAction)toggleShowHide:(id)sender
+{
+	UISegmentedControl *control = (UISegmentedControl *)sender;
+	NSInteger segment = control.selectedSegmentIndex;
+	
+	if (segment == kShowSegmentIndex) {
+		[switchView setHidden:NO];
+	}
+	else {
+		[switchView setHidden:YES];
+	}
 }
-*/
+
+
+- (IBAction)sliderChanged:(id)sender
+{
+	UISlider *slider = (UISlider *)sender;
+	int progressAsInt = (int)(slider.value + .5);
+	NSString *newText = [[NSString alloc] initWithFormat:@"%d", progressAsInt];
+	sliderLabel.text = newText;
+	[newText release];
+}
 
 - (IBAction)textFieldDoneEditing:(id)sender
 {
@@ -73,5 +106,39 @@
 - (void)dealloc {
     [super dealloc];
 }
+
+/*
+ // The designated initializer. Override to perform setup that is required before the view is loaded.
+ - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+ self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+ if (self) {
+ // Custom initialization
+ }
+ return self;
+ }
+ */
+
+/*
+ // Implement loadView to create a view hierarchy programmatically, without using a nib.
+ - (void)loadView {
+ }
+ */
+
+
+/*
+ // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+ - (void)viewDidLoad {
+ [super viewDidLoad];
+ }
+ */
+
+
+/*
+ // Override to allow orientations other than the default portrait orientation.
+ - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+ // Return YES for supported orientations
+ return (interfaceOrientation == UIInterfaceOrientationPortrait);
+ }
+ */
 
 @end
