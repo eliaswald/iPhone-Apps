2c16f8c90a33b7e7d2e93bf675c04c48304fae9a
Elias Wald <elias@fwix.com>
2011-06-03 16:37:52 -0700

    more practicing

---
+++ MultiviewTest/Classes/SwitchViewController.m
@@ -0,0 +1,89 @@
+    //
+//  SwitchViewController.m
+//  MultiviewTest
+//
+//  Created by Elias Wald on 6/3/11.
+//  Copyright 2011 Brown University. All rights reserved.
+//
+
+#import "SwitchViewController.h"
+#import "BlueViewController.h"
+#import "YellowViewController.h"
+
+@implementation SwitchViewController
+@synthesize blueViewController, yellowViewController;
+
+// The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
+/*
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization.
+    }
+    return self;
+}
+*/
+
+/*
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView {
+}
+*/
+
+
+- (void)viewDidLoad {
+    BlueViewController *blueController = [[BlueViewController alloc] initWithNibName:@"BlueView" bundle:nil];
+	self.blueViewController = blueController;
+	[self.view insertSubview:blueController.view atIndex:0];
+	[blueController release];
+}
+
+- (IBAction)switchViews:(id)sender
+{
+	if (self.yellowViewController == nil) {
+		YellowViewController *yellowController = [[YellowViewController alloc] initWithNibName:@"YellowView" bundle:nil];
+		self.yellowViewController = yellowController;
+		[yellowController release];
+	}
+	
+	if (self.blueViewController.view.superview == nil) {
+		[yellowViewController.view removeFromSuperview];
+		[self.view insertSubview:blueViewController.view atIndex:0];
+	}
+	else {
+		[blueViewController.view removeFromSuperview];
+		[self.view insertSubview:yellowViewController.view atIndex:0];
+	}
+}
+
+
+/*
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations.
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+*/
+
+- (void)didReceiveMemoryWarning {
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc. that aren't in use.
+}
+
+- (void)viewDidUnload {
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+
+- (void)dealloc {
+	[blueViewController release];
+	[yellowViewController release];
+    [super dealloc];
+}
+
+
+@end
